c579f14d95f58583f123b4a2761cbe903e61aee3
Elvinp329 <elvinkj@code.edu.az>
2023-01-05 17:09:22 +0400

    task 5-6

---
+++ Massiv 4-5-6-7/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Massiv_4_5_6_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // #region5
+            // Console.Write("Masivin uzunlugunu daxil edin");
+            // int n = int.Parse(Console.ReadLine());
+            // int[] arr= new int[n];
+            // int count =0;
+            // int sum = 0;
+
+            // for (int i = 0; i<arr.Length; i++)
+            // {
+            //     arr[i] = int.Parse(Console.ReadLine());
+
+            // }
+            //  for (int i = 0;i <arr.Length;i++)
+            // {
+            //     if (i % 2 == 0 && arr[i] % 2 == 0 )
+            //     {
+            //         count++;
+            //         sum += arr[i];
+
+            //     }
+
+
+            // }
+
+            // Console.WriteLine($"Massivin tek yerde olan cut elementlerin cem {sum} sayi {count}");
+
+            //#endregion5
+            //#region6
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr=new int[n];
+            //int count = 0;
+            //int sum = 0;  
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i]=int.Parse(Console.ReadLine());
+            //}
+            //for (int i=0; i<arr.Length; i++)
+            //{
+            //    if (i % 2 == 1  && arr[i] % 2 == 1)
+            //    {
+            //        count++;
+            //        sum += arr[i];
+            //    }
+            //}
+
+
+            //Console.WriteLine($"Massivin cut yerrde olan tek elementlerin cemi {sum} say{count}");
+            //endregion6
+
+
+
+
+
+        }
+    }
+}
